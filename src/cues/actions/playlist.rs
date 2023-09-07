0b07a0a3f2b930ccdde5b54d2e3174a6e54b37f2
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2023-09-06 19:33:45 -0500

    feat: implement playlist cue

--- src/cues/actions/playlist.rs
+++ src/cues/actions/playlist.rs
@@ -1,7 +1,11 @@
-use std::{path::PathBuf, str::FromStr, convert::Infallible, time::Duration};
+use std::{path::{PathBuf, Path}, str::FromStr, convert::Infallible, time::Duration, io::{self, BufReader}};
 
+use rodio::{Sink, Source, Decoder, source};
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
+use thiserror::Error;
+
+use crate::sound::{PrepareCue, PlaybackExecutable};
 
 #[serde_as]
 #[cfg_attr(test, derive(Eq, PartialEq))]
@@ -14,12 +18,14 @@ pub struct PlaylistCue {
     repeat: bool,
 
     #[serde(default)]
+    #[serde(alias = "loop")]
     shuffle: bool,
 
     #[serde_as(as = "Option<serde_with::DurationSecondsWithFrac>")]
     crossfade: Option<Duration>,
 }
 
+#[allow(dead_code)]
 impl PlaylistCue {
     pub fn from_folder(folder: PathBuf) -> Self {
         Self {
@@ -42,8 +48,8 @@ impl PlaylistCue {
         self
     }
 
-    pub fn folder(&self) -> Option<&PathBuf> {
-        self.folder.as_ref()
+    pub fn folder(&self) -> Option<&Path> {
+        self.folder.as_ref().map(AsRef::as_ref)
     }
 
     pub fn with_repeat(mut self) -> Self {
@@ -64,3 +70,61 @@ impl FromStr for PlaylistCue {
         Ok(Self::from_folder(PathBuf::from(s)))
     }
 }
+
+#[derive(Debug, Error)]
+pub enum PlaylistCueError {
+    #[error(transparent)]
+    File(#[from] io::Error),
+
+    #[error("Decoder error: {0}")]
+    Decode(#[from] rodio::decoder::DecoderError)
+}
+
+impl PrepareCue for PlaylistCue {
+    type Executable = PlaybackExecutable;
+
+    type PrepareError = PlaylistCueError;
+
+    fn prepare(&self, label: Option<&str>) -> Result<Self::Executable, Self::PrepareError> {
+        let mut files = self.files.clone();
+
+        if let Some(folder) = self.folder().to_owned() {
+            files.extend(folder.read_dir()?.filter_map(|entry| {
+                Some(entry.ok()?.path())
+            }));
+        }
+
+        let sources = files.into_iter().filter_map(|filename| {
+            let decoder = match std::fs::File::open(&filename) {
+                Err(e) => {
+                    log::warn!("Skipped file {} due to IO error: {e}", filename.display());
+                    return None
+                },
+                Ok(f) => Decoder::new(BufReader::new(f)),
+            };
+
+            let s: Box<dyn Source<Item = i16> + Send + Sync> = match decoder {
+                Ok(d) => Box::new(d),
+                Err(e) => {
+                    log::warn!("Skipped loading file {} due to audio decoding error: {e}", filename.display());
+                    return None
+                },
+            };
+
+            Some(s)
+        }).collect::<Vec<_>>();
+
+        let mut s: Box<dyn Source<Item = i16> + Send + Sync> = Box::new(source::from_iter(sources));
+
+        if self.repeat {
+            s = Box::new(s.repeat_infinite())
+        }
+
+        let (sink, queue) = Sink::new_idle();
+
+        sink.append(s);
+
+        Ok(PlaybackExecutable::new(label.map(ToString::to_string), sink, queue))
+    }
+}
+
